f0d7a5d5766c36b4871a3b66a78e53380658ca37
David Khristepher Santos <rupert.avery@gmail.com>
2022-12-12 11:28:27 +0800

    Fix bug with I keybinding preventing input of letter

---
+++ Diffusion.Toolkit/Common/BareKeyBinding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Diffusion.Toolkit.Common
+{
+    /// <summary>
+    /// This only exists because the InputBinding constructor is protected, but since we have to have it anyway
+    /// we also use this opportunity to simplify adding a BareKeyGesture to it.
+    /// </summary>
+    public class BareKeyBinding : InputBinding
+    {
+        private BareKeyGesture _gesture = new();
+
+        public BareKeyBinding()
+        {
+            Gesture = _gesture;
+        }
+
+        public Key Key
+        {
+            get => _gesture.Key;
+            set { _gesture.Key = value; }
+        }
+    }
+}
---
+++ Diffusion.Toolkit/Common/BareKeyGesture.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace Diffusion.Toolkit.Common;
+
+/// <summary>
+/// This gesture doesn't handle keys originating in a text control. This allows key bindings without modifier keys
+/// that don't break normal typing. A standard KeyGesture doesn't have such logic; this allows the parent of a
+/// text box to handle such bare keypresses before the textbox gets to see it as normal text input, thus breaking
+/// normal typing.
+/// </summary>
+public class BareKeyGesture : InputGesture
+{
+    public Key Key { get; set; }
+
+    public override bool Matches(object targetElement, InputEventArgs inputEventArgs)
+    {
+        var keyEventArgs = inputEventArgs as KeyEventArgs;
+        if (keyEventArgs == null)
+            return false;
+
+        if (inputEventArgs.OriginalSource is TextBoxBase { IsReadOnly: false })
+            return false;
+
+        return (int)Key == (int)keyEventArgs.Key && Keyboard.Modifiers == ModifierKeys.None;
+    }
+}
