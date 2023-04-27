c13a3606b54308a1034b6b9f9141ae7805e2664c
David Khristepher Santos <rupert.avery@gmail.com>
2023-04-27 16:32:52 +0800

    Improved performance loading thumbnails
    More of a regression from previous thumbnail performance, since adding folders and albums

--- Diffusion.Toolkit/Controls/ThumbnailView.xaml.cs
+++ Diffusion.Toolkit/Controls/ThumbnailView.xaml.cs
@@ -15,6 +15,38 @@ using Microsoft.Extensions.Options;
 
 namespace Diffusion.Toolkit.Controls
 {
+
+    public class ThumbnailTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement elem = container as FrameworkElement;
+            if (elem == null)
+            {
+                return null;
+            }
+            if (item == null)
+            {
+                throw new ApplicationException();
+            }
+
+            if (item is ImageEntry imageEntry)
+            {
+                var key = imageEntry.EntryType switch
+                {
+                    EntryType.File => "FileDataTemplate",
+                    EntryType.Folder => "FolderDataTemplate",
+                    EntryType.Album => "AlbumDataTemplate",
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+
+                return elem.FindResource(key) as DataTemplate;
+            }
+
+            throw new ApplicationException();
+        }
+    }
+
     /// <summary>
     /// Interaction logic for ThumbnailView.xaml
     /// </summary>
