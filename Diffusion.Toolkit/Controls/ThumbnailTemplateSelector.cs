c9055b3a4129a7f8bb136db301ba8787b0910e07
David Khristepher Santos <rupert.avery@gmail.com>
2023-04-28 07:23:48 +0800

    Cleanup

---
+++ Diffusion.Toolkit/Controls/ThumbnailTemplateSelector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Diffusion.Toolkit.Controls;
+
+public class ThumbnailTemplateSelector : DataTemplateSelector
+{
+    public override DataTemplate SelectTemplate(object item, DependencyObject container)
+    {
+        FrameworkElement elem = container as FrameworkElement;
+        if (elem == null)
+        {
+            return null;
+        }
+        if (item == null)
+        {
+            throw new ApplicationException();
+        }
+
+        if (item is ImageEntry imageEntry)
+        {
+            var key = imageEntry.EntryType switch
+            {
+                EntryType.File => "FileDataTemplate",
+                EntryType.Folder => "FolderDataTemplate",
+                EntryType.Album => "AlbumDataTemplate",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            return elem.FindResource(key) as DataTemplate;
+        }
+
+        throw new ApplicationException();
+    }
+}
