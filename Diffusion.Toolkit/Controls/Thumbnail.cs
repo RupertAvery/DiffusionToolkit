b4d2d43e6db922a95b6ff84eeeae517e4cb41286
David Khristepher Santos <rupert.avery@gmail.com>
2023-04-28 09:41:49 +0800

    Owner-drawn Icons

--- Diffusion.Toolkit/Controls/Thumbnail.cs
+++ Diffusion.Toolkit/Controls/Thumbnail.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace Diffusion.Toolkit.Controls;
 
@@ -10,10 +12,38 @@ public class Thumbnail : FrameworkElement
         DependencyProperty.Register(nameof(Source), typeof(BitmapSource), typeof(Thumbnail),
             new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
 
-    public BitmapSource Source
+    public static readonly DependencyProperty DataProperty =
+        DependencyProperty.Register(nameof(Data), typeof(ImageEntry), typeof(Thumbnail),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender, PropertyChangedCallback));
+
+    private static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var image = (ImageEntry)e.NewValue;
+        if (image != null)
+        {
+            image.PropertyChanged += (sender, args) =>
+            {
+                switch (args.PropertyName)
+                {
+                    case nameof(ImageEntry.ForDeletion):
+                        var thumb = d as Thumbnail;
+                        thumb.InvalidateVisual();
+                        break;
+                }
+            };
+        }
+    }
+
+    public BitmapSource? Source
     {
         get => (BitmapSource)GetValue(SourceProperty);
         set => SetValue(SourceProperty, value);
+    }
+
+    public ImageEntry? Data
+    {
+        get => (ImageEntry)GetValue(DataProperty);
+        set => SetValue(DataProperty, value);
     }
 
     protected override void OnRender(DrawingContext drawingContext)
@@ -42,6 +72,16 @@ public class Thumbnail : FrameworkElement
 
         var rect = new Rect((Width - width) / 2, (Height - height) / 2, width, height);
 
+        if (Data.ForDeletion)
+        {
+            drawingContext.PushOpacity(0.2);
+        }
+
         drawingContext.DrawImage(Source, rect);
+
+        if (Data.ForDeletion)
+        {
+            drawingContext.Pop();
+        }
     }
 }
---
+++ Diffusion.Toolkit/Controls/ThumbnailIcons.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Diffusion.Toolkit.Controls;
+
+public class ThumbnailIcons : FrameworkElement
+{
+    private static BitmapImage? _starIcon;
+    private static BitmapImage? _heartIcon;
+    private static Typeface _typeFace = new Typeface(new FontFamily("Arial"), FontStyles.Normal, FontWeights.Normal, FontStretches.Normal);
+
+    public static readonly DependencyProperty DataProperty =
+        DependencyProperty.Register(nameof(Data), typeof(ImageEntry), typeof(ThumbnailIcons),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender, PropertyChangedCallback));
+
+    private static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var image = (ImageEntry)e.NewValue;
+        if (image != null)
+        {
+            image.PropertyChanged += (sender, args) =>
+            {
+                switch (args.PropertyName)
+                {
+                    case nameof(ImageEntry.ForDeletion):
+                    case nameof(ImageEntry.Favorite):
+                    case nameof(ImageEntry.Rating):
+                        var thumb = d as ThumbnailIcons;
+                        thumb.InvalidateVisual();
+                        break;
+                }
+            };
+        }
+    }
+
+    public ImageEntry? Data
+    {
+        get => (ImageEntry)GetValue(DataProperty);
+        set => SetValue(DataProperty, value);
+    }
+
+    static ThumbnailIcons()
+    {
+        InitIcons();
+    }
+
+    private static void InitIcons()
+    {
+        Uri heartIconUri = new Uri("pack://application:,,,/Icons/blue-heart-32.png", UriKind.RelativeOrAbsolute);
+        _heartIcon = new BitmapImage(heartIconUri);
+        Uri starIconUri = new Uri("pack://application:,,,/Icons/star-32.png", UriKind.RelativeOrAbsolute);
+        _starIcon = new BitmapImage(starIconUri);
+        //Unloaded += OnUnloaded;
+    }
+
+
+    protected override void OnRender(DrawingContext drawingContext)
+    {
+        base.OnRender(drawingContext);
+
+        if (Data == null)
+            return;
+
+
+        var x = 0;
+        var y = 0;
+
+        if (Data.Favorite)
+        {
+            drawingContext.DrawImage(_heartIcon, new Rect(new Point(x, y), new Size(24, 24)));
+            x += 24;
+        }
+
+        if (Data.Rating.HasValue)
+        {
+            drawingContext.DrawImage(_starIcon, new Rect(new Point(x, y), new Size(24, 24)));
+            var value = Data.Rating.Value.ToString();
+            var formattedText = new FormattedText(value, CultureInfo.CurrentCulture, FlowDirection.LeftToRight, _typeFace, 14, Brushes.Black, null, TextFormattingMode.Display, 92)
+            {
+                TextAlignment = TextAlignment.Center
+            };
+            drawingContext.DrawText(formattedText, new Point(x + 16 - formattedText.WidthIncludingTrailingWhitespace / 2, y + 5));
+            x += 24;
+        }
+
+
+    }
+}
