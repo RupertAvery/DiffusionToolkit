c06293cb49fa3d39f0c9430430cc0c459a30dbe3
David Khristepher Santos <rupert.avery@gmail.com>
2024-01-12 08:40:25 -0800

    Fix sqlite3.dll not found. #180

--- Diffusion.Database/SQLite.cs
+++ Diffusion.Database/SQLite.cs
@@ -4556,7 +4556,7 @@ namespace SQLite
 			Serialized = 3
 		}
 
-		const string LibraryPath = "sqlite3";
+		const string LibraryPath = "e_sqlite3";
 
 #if !USE_CSHARP_SQLITE && !USE_WP8_NATIVE_SQLITE && !USE_SQLITEPCL_RAW
 		[DllImport(LibraryPath, EntryPoint = "sqlite3_threadsafe", CallingConvention=CallingConvention.Cdecl)]
