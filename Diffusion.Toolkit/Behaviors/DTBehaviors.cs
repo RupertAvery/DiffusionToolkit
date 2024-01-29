09aeb6021b71531d023f54373c49a5ebb4a72e6b
David Khristepher Santos <rupert.avery@gmail.com>
2024-01-28 20:59:06 -0800

    Move ScrollSpeed to behaviors

--- Diffusion.Toolkit/Behaviors/DTBehaviors.cs
+++ Diffusion.Toolkit/Behaviors/DTBehaviors.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 
 namespace Diffusion.Toolkit.Behaviors
 {
