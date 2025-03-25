7fd823fa1eb1dc8f6e6933f32e662da5f72f8123
David Khristepher Santos <rupert.avery@gmail.com>
2025-03-25 04:48:07 -0700

    Some node cleanup. Fix workflow not loading on first try

--- Diffusion.Toolkit/Controls/ComfyNodeStack.cs
+++ Diffusion.Toolkit/Controls/ComfyNodeStack.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -8,49 +9,68 @@ using Diffusion.IO;
 namespace Diffusion.Toolkit.Controls;
 public partial class ComfyNodeStack : UserControl
 {
+    public static readonly DependencyProperty NodesProperty = DependencyProperty.Register(
+        nameof(Nodes),
+        typeof(IReadOnlyCollection<Node>),
+        typeof(ComfyNodeStack),
+        new PropertyMetadata(null, PropertyChangedCallback)
+    );
+
+    readonly Dictionary<int, ComfyWorkflow> _workflows = new Dictionary<int, ComfyWorkflow>();
+
+    private static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (e.Property.Name == nameof(Nodes))
+        {
+            var instance = (ComfyNodeStack)d;
+
+            if (e.NewValue is IReadOnlyCollection<Node> nodes)
+            {
+                var hash = 0;
+
+                foreach (var node in nodes)
+                {
+                    hash = (hash * 397) ^ node.GetHashCode();
+                }
+
+                if (!instance._workflows.TryGetValue(hash, out var workflow))
+                {
+                    workflow = new ComfyWorkflow();
+                    workflow.BuildWorkflow(nodes);
+                    instance._workflows.Add(hash, workflow);
+                }
+
+                workflow.DataContext = nodes;
+                instance._contentControl.Content = workflow;
+            }
+            else
+            {
+                instance._contentControl.Content = null;
+            }
+        }
+    }
+
+        
+    public IReadOnlyCollection<Node> Nodes
+    {
+        get => (IReadOnlyCollection<Node>)GetValue(NodesProperty);
+        set => SetValue(NodesProperty, value);
+    }
+
+
     private int id;
-    private Dictionary<int, ComfyWorkflow> _workflows = new Dictionary<int, ComfyWorkflow>();
     ContentControl _contentControl;
 
     public ComfyNodeStack()
     {
         Background = new SolidColorBrush(Colors.Transparent);
-        DataContextChanged += OnDataContextChanged;
+
         _contentControl = new ContentControl()
         {
             Background = new SolidColorBrush(Colors.Transparent)
         };
+
         AddChild(_contentControl);
     }
 
-    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
-    {
-        if (e.NewValue is IReadOnlyCollection<Node> nodes)
-        {
-            var hash = 0;
-
-            foreach (var node in nodes)
-            {
-                hash = (hash * 397) ^ node.GetHashCode();
-            }
-
-            if (!_workflows.TryGetValue(hash, out var workflow))
-            {
-                workflow = new ComfyWorkflow();
-                workflow.BuildWorkflow(nodes);
-                workflow.DataContext = nodes;
-                _workflows.Add(hash, workflow);
-            }
-            else
-            {
-                workflow.DataContext = nodes;
-                _contentControl.Content = workflow;
-            }
-
-        }
-        else
-        {
-            _contentControl.Content = null;
-        }
-    }
 }
